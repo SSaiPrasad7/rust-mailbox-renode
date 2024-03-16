0cd77ea18b3d9b4c4a3cf4b53859989338036635
SSaiPrasad7 <samudralasai1999@protonmail.com>
2024-03-16 09:04:09 +0200

    feat: running hello world from two cores
    Send Hello msg from one core and World msg from another core
    on UART

--- rust/dual-core/core0/src/main.rs
+++ rust/dual-core/core0/src/main.rs
@@ -6,6 +6,12 @@ use panic_halt as _;
 use riscv_rt::entry;
 use core::ptr;
 
+pub mod shared {
+    #[link_section = ".shared_data"]
+    #[no_mangle]
+    pub static mut SHARED_VALUE: &'static str = "Hello";
+}
+
 #[entry]
 fn main() -> ! {
     let clk_freq = 30000000;
@@ -18,12 +24,11 @@ fn main() -> ! {
     let uart_tx_cfg_offset = 0x18;
     let uart_setup_offset = 0x24;
     let uart_config = (uart_clk_div << 16) | 0x317;
-    let s = "\r\nHello World\r\n";
 
     unsafe {
         ptr::write_volatile((uart_base_addr + uart_setup_offset) as *mut u32, uart_config);
-        ptr::write_volatile((uart_base_addr + uart_tx_saddr_offset) as *mut u32, s.as_ptr() as u32);
-        ptr::write_volatile((uart_base_addr + uart_tx_size_offset) as *mut u32, s.len() as u32);
+        ptr::write_volatile((uart_base_addr + uart_tx_saddr_offset) as *mut u32, shared::SHARED_VALUE.as_ptr() as u32);
+        ptr::write_volatile((uart_base_addr + uart_tx_size_offset) as *mut u32, shared::SHARED_VALUE.len() as u32);
         ptr::write_volatile((uart_base_addr + uart_tx_cfg_offset) as *mut u32, 1 << 4);
         let value = ptr::read_volatile((uart_base_addr + uart_tx_saddr_offset) as *mut u32);
         while value != 0 {}
