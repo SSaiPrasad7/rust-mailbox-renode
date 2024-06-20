3cd7c887c3a2d7bd5be1bcc8baa622301b9ddbce
SSaiPrasad7 <samudralasai1999@protonmail.com>
2024-06-20 20:00:23 +0300

    chore: implement basic mailbox without interrupt
    With current implementation, RAM is taken use by the two
    cores by dividing the address space of the 512KiB into two 256 kiB.
    The mailbox writes into shared memory from one core0 and after certain
    amount of time the value is read from shared memory by core1.

--- rust/dual-core/core0/src/main.rs
+++ rust/dual-core/core0/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use core::ptr;
 use panic_halt as _;
 use riscv_rt::entry;
 use udma_uart;
@@ -8,13 +9,18 @@ use udma_uart;
 #[entry]
 fn main() -> ! {
     let baud_rate = 115200;
-    const UART_BASE_ADDR: usize = 0x1a10_2080;
+    const UART_BASE_ADDR: usize = 0x1A10_2080;
+    const SHARED_MEM_ADDR: usize = 0x1C08_0000;
+
     use udma_uart::Uart;
     let _uart = Uart;
-
     udma_uart::udma_uart_init(UART_BASE_ADDR, baud_rate);
 
-    udma_uart::sprintln!("Hello {}", "world");
-    udma_uart::sprintln!("Here's some numbers {}, {}, {}", 1, 2, 3);
+    let value = 42;
+    unsafe {
+        udma_uart::sprintln!("Core 0: Writing value {} to shared memory", value);
+        ptr::write_volatile(SHARED_MEM_ADDR as *mut u32, value);
+    }
+
     loop {}
 }
--- rust/dual-core/core1/src/main.rs
+++ rust/dual-core/core1/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
+use core::ptr;
 use panic_halt as _;
 use riscv_rt::entry;
 use udma_uart;
@@ -8,13 +10,21 @@ use udma_uart;
 #[entry]
 fn main() -> ! {
     let baud_rate = 115200;
-    const UART_BASE_ADDR: usize = 0x1a10_2100;
+    const UART_BASE_ADDR: usize = 0x1A10_2100;
+    const SHARED_MEM_ADDR: usize = 0x1C08_0000;
+
     use udma_uart::Uart;
     let _uart = Uart;
-
     udma_uart::udma_uart_init(UART_BASE_ADDR, baud_rate);
 
-    udma_uart::sprintln!("Hello {}", "world");
-    udma_uart::sprintln!("Here's some numbers {}, {}, {}", 1, 2, 3);
+    for _ in 0..1000000 {
+        unsafe { asm!("nop") }
+    }
+
+    unsafe {
+        let value = ptr::read_volatile((SHARED_MEM_ADDR) as *mut u32);
+        udma_uart::sprintln!("Core 1: Reading value {} from shared memory", value);
+    }
+
     loop {}
 }
