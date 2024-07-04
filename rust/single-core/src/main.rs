07a43d0298afa719138ff25405273dbcee5cb275
SSaiPrasad7 <samudralasai1999@protonmail.com>
2024-07-04 22:37:29 +0300

    chore: trigger interrupt continuously for single core
    Triggers interrupt after certain amount of delay and interrupt
    handler on the other side prints message that indicates that
    correct interrupt is raised.

--- rust/single-core/src/main.rs
+++ rust/single-core/src/main.rs
@@ -1,15 +1,25 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::asm, ptr::write_volatile};
+use core::{ptr::read_volatile, ptr::write_volatile};
 use panic_halt as _;
-use riscv::register::{mcause, mie, mstatus, mtvec};
+use riscv::{
+    asm::delay,
+    register::{mcause, mie, mstatus, mtvec},
+};
 use riscv_rt::entry;
 use udma_uart::Uart;
 
 const ITC_BASE_ADDR: usize = 0x1A10_9000;
+const SW_INT_ID: u8 = 3;
+
+const _ITC_MASK_OFFSET: usize = 0x00;
 const ITC_MASK_SET_OFFSET: usize = 0x04;
+const _ITC_MASK_CLR_OFFSET: usize = 0x08;
+
+const ITC_INT_OFFSET: usize = 0x0C;
 const ITC_INT_SET_OFFSET: usize = 0x10;
+const ITC_INT_CLR_OFFSET: usize = 0x14;
 
 unsafe fn init_mtvec() {
     extern "C" {
@@ -23,10 +33,12 @@ unsafe extern "C" fn _msoft_int_handler() {
     match mcause::read().cause() {
         mcause::Trap::Interrupt(mcause::Interrupt::MachineSoft) => {
             udma_uart::sprintln!("Hello from {}", "Interrupt Handler");
+
+            // Clear machine software interrupt in ITC PULP IRQ INTERRUPT register
+            let interrupt_clr_reg: *mut u32 = (ITC_BASE_ADDR + ITC_INT_CLR_OFFSET) as *mut u32;
+            write_volatile(interrupt_clr_reg, 1 << SW_INT_ID);
         }
-        _ => {
-            udma_uart::sprintln!("Not Machine Soft Interrupt");
-        }
+        _ => {}
     }
 }
 
@@ -35,22 +47,31 @@ fn main() -> ! {
     let baud_rate = 115200;
     const UART_BASE_ADDR: usize = 0x1A10_2100;
     udma_uart::udma_uart_init(UART_BASE_ADDR, baud_rate);
-    udma_uart::sprintln!("Hello {}", "from main function");
+    // udma_uart::sprintln!("Hello from {}", "main");
+
     unsafe {
         init_mtvec();
-        // Enable machine software interrupts and global machine interrupts
+        // Enable machine software interrupts
         mie::set_msoft();
-        mstatus::set_mie();
-
-        const SW_INT_ID: u8 = 3;
+        // SET machine software interrupt in ITC PULP IRQ MASK register
         let mask_set_reg = (ITC_BASE_ADDR + ITC_MASK_SET_OFFSET) as *mut u32;
         write_volatile(mask_set_reg, 1 << SW_INT_ID);
-
-        let interrupt_set_reg = (ITC_BASE_ADDR + ITC_INT_SET_OFFSET) as *mut u32;
-        write_volatile(interrupt_set_reg, 1 << SW_INT_ID);
     }
 
     loop {
-        unsafe { asm!("nop") }
+        unsafe {
+            // Extract and check interrupt pending or not for machine software interrupt
+            let int_reg_value = read_volatile((ITC_BASE_ADDR + ITC_INT_OFFSET) as *mut u32);
+            if (int_reg_value >> SW_INT_ID) & 1 == 0 {
+                delay(37_000_000);
+
+                // Set machine software interrupt in ITC PULP IRQ INTERRUPT register
+                let interrupt_set_reg: *mut u32 = (ITC_BASE_ADDR + ITC_INT_SET_OFFSET) as *mut u32;
+                write_volatile(interrupt_set_reg, 1 << SW_INT_ID);
+
+                // Enable global machine interrupts
+                mstatus::set_mie();
+            }
+        }
     }
 }
