75fe8caaa70e9032c6db7bfea952348a931ea53a
SSaiPrasad7 <samudralasai1999@protonmail.com>
2024-07-08 21:14:41 +0300

    wip: test bidirectional mailbox

--- rust/dual-core/core0/src/main.rs
+++ rust/dual-core/core0/src/main.rs
@@ -3,7 +3,10 @@
 
 use core::{ptr::read_volatile, ptr::write_volatile};
 use panic_halt as _;
-use riscv::{asm::delay, register::mstatus};
+use riscv::{
+    // asm::delay,
+    register::{mcause, mie, mstatus, mtvec},
+};
 use riscv_rt::entry;
 use udma_uart::Uart;
 
@@ -11,14 +14,43 @@ const SHARED_MEM_ADDR: usize = 0x1C08_0000;
 
 const ITC_BASE_ADDR: usize = 0x1A10_9000;
 const SW_INT_ID: u8 = 3;
+const _EXT_INT_ID: u8 = 7;
 
 const _ITC_MASK_OFFSET: usize = 0x00;
 const ITC_MASK_SET_OFFSET: usize = 0x04;
 const _ITC_MASK_CLR_OFFSET: usize = 0x08;
 
-const ITC_INT_OFFSET: usize = 0x0C;
+const _ITC_INT_OFFSET: usize = 0x0C;
 const ITC_INT_SET_OFFSET: usize = 0x10;
 const _ITC_INT_CLR_OFFSET: usize = 0x14;
+
+unsafe fn init_mtvec() {
+    extern "C" {
+        fn _mext_int_handler();
+    }
+    mtvec::write(_mext_int_handler as usize, mtvec::TrapMode::Direct);
+}
+
+#[no_mangle]
+unsafe extern "C" fn _mext_int_handler() {
+    match mcause::read().cause() {
+        mcause::Trap::Interrupt(mcause::Interrupt::MachineExternal) => {
+            // udma_uart::sprintln!("Core 0: Hello from {}", "Interrupt Handler");
+            
+            let value = read_volatile((SHARED_MEM_ADDR) as *mut u32);
+            udma_uart::sprintln!("Core 0: Reading value {} from shared memory.", value);
+            write_volatile(SHARED_MEM_ADDR as *mut u32, value + 1);
+
+            // Trigger the machine software interrupt
+            let interrupt_set_reg: *mut u32 = (ITC_BASE_ADDR + ITC_INT_SET_OFFSET) as *mut u32;
+            write_volatile(interrupt_set_reg, 1 << SW_INT_ID);
+
+            // Enable global machine interrupts again
+            mstatus::set_mie();
+        }
+        _ => {}
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -28,31 +60,26 @@ fn main() -> ! {
     udma_uart::sprintln!("Hello from {}", "Core 0");
 
     unsafe {
+        // Register interrupt handler for machine external interrupt
+        init_mtvec();
+        // Enable machine external interrupts
+        mie::set_mext();
+
         // SET machine software interrupt in ITC PULP IRQ MASK register
-        let mask_set_reg = (ITC_BASE_ADDR + ITC_MASK_SET_OFFSET) as *mut u32;
+        let mask_set_reg: *mut u32 = (ITC_BASE_ADDR + ITC_MASK_SET_OFFSET) as *mut u32;
         write_volatile(mask_set_reg, 1 << SW_INT_ID);
+
+        let write_value = 42;
+        udma_uart::sprintln!("Core 0: Writing value {} to shared memory.", write_value);
+        write_volatile(SHARED_MEM_ADDR as *mut u32, write_value);
+
+        // Trigger the machine software interrupt
+        let interrupt_set_reg: *mut u32 = (ITC_BASE_ADDR + ITC_INT_SET_OFFSET) as *mut u32;
+        write_volatile(interrupt_set_reg, 1 << SW_INT_ID);
+
+        // Enable global machine interrupts
+        mstatus::set_mie();
     }
 
-    let mut write_value = 42;
-
-    loop {
-        unsafe {
-            // Extract and check interrupt pending or not for machine software interrupt
-            let int_reg_value = read_volatile((ITC_BASE_ADDR + ITC_INT_OFFSET) as *mut u32);
-
-            if (int_reg_value >> SW_INT_ID) & 1 == 0 {
-                udma_uart::sprintln!("Core 0: Writing value {} to shared memory.", write_value);
-                delay(37_000_000);
-                write_volatile(SHARED_MEM_ADDR as *mut u32, write_value);
-                write_value += 1;
-
-                // Trigger the interrupt
-                let interrupt_set_reg: *mut u32 = (ITC_BASE_ADDR + ITC_INT_SET_OFFSET) as *mut u32;
-                write_volatile(interrupt_set_reg, 1 << SW_INT_ID);
-
-                // Enable global machine interrupts
-                mstatus::set_mie();
-            }
-        }
-    }
+    loop {}
 }
--- rust/dual-core/core1/src/main.rs
+++ rust/dual-core/core1/src/main.rs
@@ -13,15 +13,16 @@ use udma_uart::Uart;
 const SHARED_MEM_ADDR: usize = 0x1C08_0000;
 
 const ITC_BASE_ADDR: usize = 0x1A10_9000;
-const SW_INT_ID: u8 = 3;
+const _SW_INT_ID: u8 = 3;
+const EXT_INT_ID: u8 = 7;
 
 const _ITC_MASK_OFFSET: usize = 0x00;
-const _ITC_MASK_SET_OFFSET: usize = 0x04;
+const ITC_MASK_SET_OFFSET: usize = 0x04;
 const _ITC_MASK_CLR_OFFSET: usize = 0x08;
 
 const _ITC_INT_OFFSET: usize = 0x0C;
-const _ITC_INT_SET_OFFSET: usize = 0x10;
-const ITC_INT_CLR_OFFSET: usize = 0x14;
+const ITC_INT_SET_OFFSET: usize = 0x10;
+const _ITC_INT_CLR_OFFSET: usize = 0x14;
 
 unsafe fn init_mtvec() {
     extern "C" {
@@ -35,12 +36,14 @@ unsafe extern "C" fn _msoft_int_handler() {
     match mcause::read().cause() {
         mcause::Trap::Interrupt(mcause::Interrupt::MachineSoft) => {
             // udma_uart::sprintln!("Core 1: Hello from {}", "Interrupt Handler");
+            
             let value = read_volatile((SHARED_MEM_ADDR) as *mut u32);
             udma_uart::sprintln!("Core 1: Reading value {} from shared memory.", value);
+            write_volatile(SHARED_MEM_ADDR as *mut u32, value + 1);
 
-            // Clear machine software interrupt in ITC PULP IRQ INTERRUPT register
-            let interrupt_clr_reg = (ITC_BASE_ADDR + ITC_INT_CLR_OFFSET) as *mut u32;
-            write_volatile(interrupt_clr_reg, 1 << SW_INT_ID);
+            // Trigger external interrupt
+            let interrupt_set_reg: *mut u32 = (ITC_BASE_ADDR + ITC_INT_SET_OFFSET) as *mut u32;
+            write_volatile(interrupt_set_reg, 1 << EXT_INT_ID);
 
             // Enable global machine interrupts again
             mstatus::set_mie();
@@ -59,9 +62,14 @@ fn main() -> ! {
     unsafe {
         // Register interrupt handler for machine software interrupt
         init_mtvec();
-
-        // Enable machine software interrupts and global machine interrupts
+        // Enable machine software interrupts
         mie::set_msoft();
+
+        // SET machine external interrupt in ITC PULP IRQ MASK register
+        let mask_set_reg = (ITC_BASE_ADDR + ITC_MASK_SET_OFFSET) as *mut u32;
+        write_volatile(mask_set_reg, 1 << EXT_INT_ID);
+
+        // Enable global machine interrupts
         mstatus::set_mie();
     }
 
