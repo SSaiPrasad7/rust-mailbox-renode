080cd6a189f4c1444e1b39238879e2e4e0c4d279
SSaiPrasad7 <samudralasai1999@protonmail.com>
2024-10-21 16:56:19 +0300

    feat: modify linker scripts for communication b/w cores using SM
    - Added a 2 KB buffer in shared memory to facilitate data exchange
      between two cores.
    - Configured linker scripts to allocate and manage shared memory(SM)
      effectively.
    - Improved inter-core communication efficiency and reliability
      with this implementation.

--- rust/dual-core/core1/src/main.rs
+++ rust/dual-core/core1/src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
+#![allow(unused)]
+#![allow(static_mut_refs)]
 
-use core::{ptr::read_volatile, ptr::write_volatile};
+use core::ptr::write_volatile;
 use panic_halt as _;
 use riscv::{
     // asm::delay,
@@ -10,19 +12,31 @@ use riscv::{
 use riscv_rt::entry;
 use udma_uart::Uart;
 
-const SHARED_MEM_ADDR: usize = 0x1C08_0000;
-
 const ITC_BASE_ADDR: usize = 0x1A10_9000;
 const SW_INTERRUPT_ID_16: u8 = 16;
 const SW_INTERRUPT_ID_17: u8 = 17;
 
-const _ITC_MASK_OFFSET: usize = 0x00;
+const ITC_MASK_OFFSET: usize = 0x00;
 const ITC_MASK_SET_OFFSET: usize = 0x04;
-const _ITC_MASK_CLR_OFFSET: usize = 0x08;
+const ITC_MASK_CLR_OFFSET: usize = 0x08;
 
-const _ITC_INT_OFFSET: usize = 0x0C;
+const ITC_INT_OFFSET: usize = 0x0C;
 const ITC_INT_SET_OFFSET: usize = 0x10;
 const ITC_INT_CLR_OFFSET: usize = 0x14;
+const MAILBOX_BUFFER_SIZE: usize = 2048; // 2KB
+
+#[link_section = ".shared"]
+static mut MAILBOX: [u32; MAILBOX_BUFFER_SIZE] = [0; MAILBOX_BUFFER_SIZE];
+
+fn write_to_mailbox(data: u32, index: usize) {
+    unsafe {
+        MAILBOX[index] = data;
+    }
+}
+
+fn read_from_mailbox(index: usize) -> u32 {
+    unsafe { MAILBOX[index] }
+}
 
 unsafe fn init_mtvec() {
     extern "C" {
@@ -36,21 +50,23 @@ unsafe extern "C" fn _mfast_soft_int_handler() {
     match mcause::read().cause() {
         mcause::Trap::Interrupt(mcause::Interrupt::MachineFast16) => {
             // udma_uart::sprintln!("Core 1: Hello from {}", "Interrupt Handler");
-
-            let value = read_volatile((SHARED_MEM_ADDR) as *mut u32);
-            udma_uart::sprintln!("Core 1: Reading value {} from shared memory.", value);
-            write_volatile(SHARED_MEM_ADDR as *mut u32, value + 1);
+            // let value = read_from_mailbox(1);
+            // udma_uart::sprintln!("Core 1: Reading value {} from shared memory.", value);
+            // write_to_mailbox(value + 1, 1);
+            udma_uart::sprintln!("Core 1: Reading the mailbox:");
+            udma_uart::sprintln!("{:?}", MAILBOX);
 
             // Clear machine fast software interrupt number 16
             let interrupt_clr_reg = (ITC_BASE_ADDR + ITC_INT_CLR_OFFSET) as *mut u32;
             write_volatile(interrupt_clr_reg, 1 << SW_INTERRUPT_ID_16);
 
-            // Trigger machine fast software interrupt number 17
-            let interrupt_set_reg: *mut u32 = (ITC_BASE_ADDR + ITC_INT_SET_OFFSET) as *mut u32;
-            write_volatile(interrupt_set_reg, 1 << SW_INTERRUPT_ID_17);
+            // // Trigger machine fast software interrupt number 17
+            // let interrupt_set_reg: *mut u32 = (ITC_BASE_ADDR +
+            // ITC_INT_SET_OFFSET) as *mut u32;
+            // write_volatile(interrupt_set_reg, 1 << SW_INTERRUPT_ID_17);
 
-            // Enable global machine interrupts again
-            mstatus::set_mie();
+            // // Enable global machine interrupts again
+            // mstatus::set_mie();
         }
         _ => {}
     }
